17e2befb4fb04616dd3227b7b96c460c6fa85597
arju88nair <arju88nair@gmail.com>
2018-05-13 19:46:01 +0530

    fb login working

--- src/config/auth.js
+++ src/config/auth.js
@@ -2,12 +2,8 @@ import { FBLoginManager } from 'react-native-facebook-login';
 
 const Facebook = {
   login: (permissions) => {
-      console.log("E");
-
     return new Promise((resolve, reject) => {
-        console.log("D");
       FBLoginManager.loginWithPermissions(permissions || ['email'], (error, data) => {
-          console.log("S");
         if (!error) {
           console.log(data);
           resolve(data.credentials.token);
