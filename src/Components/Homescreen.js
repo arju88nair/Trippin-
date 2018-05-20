303cffc9e02c2790ab9e3e1b9b0f5280b6095a84
arju88nair <arju88nair@gmail.com>
2018-05-19 20:00:00 -0600

    HOmescreen button

--- src/Components/Homescreen.js
+++ src/Components/Homescreen.js
@@ -9,6 +9,7 @@ export default class HomeScreen extends React.Component {
         return (
             <View style={styles.container}>
                 <Button title="Show me more of the app" onPress={this._showMoreApp} />
+                {/*<Button title="Actually, sign me out :)" onPress={this._signOutAsync} />*/}
             </View>
         );
     }
