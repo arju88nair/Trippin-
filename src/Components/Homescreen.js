112ee9a42c87ab091621f5c6e3f32a40052c5314
arju88nair <arju88nair@gmail.com>
2018-05-26 20:00:00 -0600

    Changed handlelogin

--- src/Components/Homescreen.js
+++ src/Components/Homescreen.js
@@ -9,14 +9,13 @@ export default class HomeScreen extends React.Component {
         return (
             <View style={styles.container}>
                 <Button title="Show me more of the app" onPress={this._showMoreApp} />
-                <Button title="Actually, sign me out :)" onPress={this._signOutAsync} />
+                {/*<Button title="Actually, sign me out :)" onPress={this._signOutAsync} />*/}
             </View>
         );
     }
 
 
     _signOutAsync = async () => {
-        await AsyncStorage.clear();
         this.props.navigation.navigate('Auth');
     };
 }
--- src/lib/auth/index.js
+++ src/lib/auth/index.js
@@ -4,13 +4,14 @@ import Auth from '../../config/auth';
 import Homescreen from '../../Components/Homescreen'
 
 export const handleFbLogin = () => (
-  Auth.Facebook.login(fbLoginPermissions)
-    .then((token) => {
-      firebase.auth()
-        .signInWithCredential(firebase.auth.FacebookAuthProvider.credential(token))
-        this.props.navigation.navigate('Home');
-
-
-    })
+    this.props.navigation.navigate('Home')
+  // Auth.Facebook.login(fbLoginPermissions)
+  //   .then((token) => {
+  //     firebase.auth()
+  //       .signInWithCredential(firebase.auth.FacebookAuthProvider.credential(token))
+  //       this.props.navigation.navigate('Home');
+  //
+  //
+  //   })
     .catch((err) => this.onError && this.onError(err))
 );
